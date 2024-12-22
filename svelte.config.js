5dba7921e679e76ec104b6aa92ff93d8ac4bf52d
sandipkarmokar <skarmokar987@gmail.com>
2024-12-22 22:33:24 +0530

    feat: enhance svelte configuration with routing and aliasing

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-/** @type {import('@sveltejs/kit').Config} */
+
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
+  preprocess: [vitePreprocess()],
   kit: {
     adapter: adapter(),
+    files: {
+      routes: "src/app/routes", // move routing inside the app layer
+      lib: "src",
+      appTemplate: "src/app/index.html", // Move the application entry point inside the app layer
+      assets: "public",
+    },
+    alias: {
+      "@/*": "src/*", // Create an alias for the src directory
+    },
   },
-  preprocess: vitePreprocess(),
 };
 export default config;
